f029b9a448423208094f490b1637c93c00507a39
Levente <suvegjartolevente@gmail.com>
2026-02-28 15:43:12 +0100

    only space checker done

--- errors/validationError.js
+++ errors/validationError.js
@@ -1,7 +1,12 @@
 export const validationError = (datas) => {
   const missingValues = [];
   for (const [key, value] of Object.entries(datas)) {
-    if (value === undefined || value === null || value === "") {
+    if (
+      value === undefined ||
+      value === null ||
+      value === "" ||
+      /^ *$/.test(value)
+    ) {
       missingValues.push(key);
     }
   }
